db47a7e3f1a756a158f3438f7aef00720d697baa
Vinukonda Praveen kumar <vinukondapraveenkumar21@gmail.com>
2026-06-23 22:49:31 +0530

    debug smtp config

--- server/services/notification.service.ts
+++ server/services/notification.service.ts
@@ -19,6 +19,10 @@ export class NotificationService {
     const pass = process.env.SMTP_PASS;
 
     if (host && user && pass) {
+      console.log("SMTP_HOST =", process.env.SMTP_HOST);
+      console.log("SMTP_PORT =", process.env.SMTP_PORT);
+      console.log("SMTP_USER =", process.env.SMTP_USER);
+      console.log("SMTP_PASS EXISTS =", !!process.env.SMTP_PASS);
       return nodemailer.createTransport({
         host,
         port,
